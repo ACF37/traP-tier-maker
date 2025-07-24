09153f5c58be79db79e22fb73cfcb9a104af0483
quaran <skrsoma37@gmail.com>
2025-07-24 22:03:46 +0900

    基本レイアウトコンポーネントの作成

---
+++ src/components/TierList.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const tiers = ref<string[]>(['S', 'A', 'B', 'C', 'D'])
+</script>
+
+<template>
+  <div class="tier-list-container">
+    <div v-for="tier in tiers" :key="tier" class="tier-row">
+      <div class="tier-label">{{ tier }}</div>
+      <div class="tier-dropzone"></div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.tier-row {
+  display: flex;
+  align-items: center;
+  margin-bottom: 5px;
+}
+
+.tier-label {
+  width: 50px;
+  height: 80px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-weight: bold;
+  background-color: #eee;
+  border: 1px solid #ccc;
+}
+
+.tier-dropzone {
+  flex-grow: 1;
+  border: 1px solid #ccc;
+  min-height: 80px;
+}
+</style>
