a8c3445c0bc3a6954d740a367289d610fd3659fd
quaran <skrsoma37@gmail.com>
2025-07-26 00:52:28 +0900

    バージョン更新時のリセットを追加

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,21 @@ import { createPersistedState } from 'pinia-plugin-persistedstate'
 import App from './App.vue'
 import './index.css'
 
+const CURRENT_VERSION = '1.1.0'
+const VERSION_KEY = 'app-version'
+const PINIA_KEY = 'icon'
+
+function checkVersion() {
+  const savedVersion = localStorage.getItem(VERSION_KEY)
+  if (savedVersion !== CURRENT_VERSION) {
+    localStorage.removeItem(PINIA_KEY)
+
+    localStorage.setItem(VERSION_KEY, CURRENT_VERSION)
+  }
+}
+
+checkVersion()
+
 const app = createApp(App)
 const pinia = createPinia()
 
